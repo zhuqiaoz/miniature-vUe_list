bc72386d94515f09169e70c2b0e09bb1f20dc4a0
BriDge <xzg_3210@hotmail.com>
2017-06-26 16:32:23 +0800

    mockjs

--- src/api/api.js
+++ src/api/api.js
@@ -2,4 +2,12 @@ import axios from 'axios'
 
 
 
-export const Listitem 
+export const Listitem  = params =>{
+    return axios.get('/api/Listitem',{
+        params: params
+    });
+};
+
+export const pl =params =>{
+    return axios.post('/api/Listitems',params).then(res => res.data)
+}
